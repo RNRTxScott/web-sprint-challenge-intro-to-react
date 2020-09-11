1c96df9e10d8fa91ae5a9a408ecea960e66e9c03
Scott Vanderburg <s.vanderburg@realnerdsrt.com>
2020-09-11 09:25:19 -0600

    added in styling dependancies and have created my axios get to the api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Characters from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const [swChar, setSwChar] = useState([])
+  useEffect(() => {
+    Axios.get('https://swapi.dev/api/people/')
+    .then(response =>{
+      setSwChar(response.data);
+    })
+    .catch(error => {
+      console.log('Broken Data', error);
+    })
+  },[])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
 
   return (
     <div className="App">
@@ -17,3 +25,17 @@ const App = () => {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
